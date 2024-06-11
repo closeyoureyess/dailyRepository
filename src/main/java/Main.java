a34425e87566352b845138453c639d547522f18b
mxnv <vadim.beller@mail.ru>
2024-06-11 22:35:42 +0300

    master - dev arraylist#3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,19 @@ public class Main {
         Solution solution = new Solution();
 
         solution.relativeSortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6});
-/*
-        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        /*ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayList2 = new ArrayList<>();
+        arrayList2.add(5);
+        arrayList2.add(9);
+
         arrayList.add(2);
         arrayList.add(4);
         arrayList.add(6);
         arrayList.add(2);
 
-        Collections.swap(arrayList, );
-        Collections.s*/
+        arrayList.add(1, arrayList.get(3));
+        arrayList.remove(arrayList.indexOf());
+        System.out.println(arrayList);*/
     }
 }
--- src/main/java/task1122/Solution.java
+++ src/main/java/task1122/Solution.java
@@ -1,9 +1,6 @@
 package task1122;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Solution {
@@ -16,21 +13,59 @@ public class Solution {
                 .boxed()
                 .collect(Collectors.toCollection(() -> new ArrayList<>()));
 
+        ArrayList<Integer> delElements = Arrays.stream(arr1)
+                .boxed()
+                .collect(Collectors.toCollection(() -> new ArrayList<>()));
+
         ArrayList<Integer> arrayList2 = Arrays.stream(arr2)
                 .boxed()
                 .collect(Collectors.toCollection(() -> new ArrayList<>()));
 
-        ArrayList<Integer> arrayList2dub = Arrays.stream(arr2)
+        delElements.removeAll(arrayList2);
+        Collections.sort(delElements);
+        System.out.println(delElements);
+
+        arrayList1.removeAll(delElements);
+        arrayList1.addAll(delElements);
+
+        System.out.println(arrayList1);
+
+        HashMap<Integer, Integer> hashMap1 = new HashMap<>();
+
+
+        for (int i = 0; i < arr2.length; i++) {
+            hashMap1.put(i, arr2[i]);
+        }
+
+        HashMap<Integer, Integer> hashMap2 = new HashMap<>();
+
+        ArrayList<Integer> needDelete = new ArrayList<>();
+
+      /*  HashMap<Integer> arrayList1 = ArrayList.stream(arr1)
                 .boxed()
-                .collect(Collectors.toCollection(() -> new ArrayList<>()));
+                .collect(Collectors.toMap());*/
 
-        //Добавление одной коллекции в другую, теперь 2,1,4,3,9,6 + 2,3,1,3,2,4,6,7,9,2,19
-        arrayList2.addAll(arrayList1);                // 2,1,4,3,9,6,2,2,2
+        /*ArrayList<Integer> arrayList2 = Arrays.stream(arr2)
+                .boxed()
+                .collect(Collectors.toCollection(() -> new ArrayList<>()));*/
+        //Добавление одной коллекции в другую, теперь 2,3,1,3,2,4,6,7,9,2,19 +  2,1,4,3,9,6
+        //arrayList2.addAll(arrayList1);                // 2, 3, 1, 3, 2, 4, 6, 9, 2, 7
+        // 2, 2, 3, 1, 3, 4, 6, 9, 2, 7
 
-        Iterator<Integer> iterator = arrayList2.iterator();
+        // С помощью add, перебрать элементы
+        // Hashmap - сравнить элементы, убедиться, что у
 
-        for (int i = 0; i < arrayList2.size(); i++) {
-            int value1 = arrayList2.get(i);
+       /* for (int i = 0; i < arrayList1.size(); i++) {
+            int valArr1 = arrayList1.get(i);
+            for (int j = 0; j < arrayList1.size(); j++){
+                if(valArr1 == arrayList1.get(j)){
+                    break;
+                } else if () {
+                    
+                }
+            }
+        }*/
+            /*int value1 = arrayList2.get(i);
             int k = i;
             System.out.println("i " + arrayList2.get(i));
             for (int j = i + 1; j < arrayList2.size(); j++) {
@@ -38,8 +73,28 @@ public class Solution {
                     Collections.swap(arrayList2, k + 1, j);
                 }
             }
+        }*/
+
+        Stack<Integer> stack = new Stack<>();
+
+
+        for (int i = 0; i < arrayList1.size() - delElements.size(); i++) { // 2,1,4,3,9,6
+            if (arrayList1.get(i).equals(hashMap1.get(i))) {               // 2, 3, 1, 3, 2, 4, 6, 9, 2, 7
+                //ищем, есть ли еще элементы-дубли  массиве
+                for (int j = i + 1; j < arrayList1.size() - delElements.size(); i++) {
+                    if (arrayList1.get(j).equals(arrayList1.get(i))) {
+                        hashMap2.put(i + 1, arrayList1.get(j));
+                        needDelete.add(j);
+                        /*arrayList1.add(i + 1, arrayList1.get(j));
+                        arrayList1.remove(j);*/
+                    }
+                }
+            } else {
+
+            }
         }
-        System.out.println("Result " + arrayList2);
+
+
         return abc;
     }
 }
