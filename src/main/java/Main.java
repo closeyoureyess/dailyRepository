dd67624600c67c8919b04c232ac11be5ff768cc3
mxnv <vadim.beller@mail.ru>
2024-06-11 11:15:48 +0300

    master - dev arraylist#2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args){
+import task1122.Solution;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.relativeSortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6});
+/*
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(4);
+        arrayList.add(6);
+        arrayList.add(2);
+
+        Collections.swap(arrayList, );
+        Collections.s*/
     }
 }
--- src/main/java/task1122/Solution.java
+++ src/main/java/task1122/Solution.java
@@ -3,11 +3,14 @@ package task1122;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.stream.Collectors;
 
 public class Solution {
 
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+
+        int[] abc = {1, 2, 3};
 
         ArrayList<Integer> arrayList1 = Arrays.stream(arr1)
                 .boxed()
@@ -17,8 +20,26 @@ public class Solution {
                 .boxed()
                 .collect(Collectors.toCollection(() -> new ArrayList<>()));
 
-        //Добавление одной коллекции в другую, теперь 2,1,4,3,9,6 + 2,3,1,3,2,4,6,7,9,2,19
-        arrayList2.addAll(arrayList1);
+        ArrayList<Integer> arrayList2dub = Arrays.stream(arr2)
+                .boxed()
+                .collect(Collectors.toCollection(() -> new ArrayList<>()));
 
+        //Добавление одной коллекции в другую, теперь 2,1,4,3,9,6 + 2,3,1,3,2,4,6,7,9,2,19
+        arrayList2.addAll(arrayList1);                // 2,1,4,3,9,6,2,2,2
+
+        Iterator<Integer> iterator = arrayList2.iterator();
+
+        for (int i = 0; i < arrayList2.size(); i++) {
+            int value1 = arrayList2.get(i);
+            int k = i;
+            System.out.println("i " + arrayList2.get(i));
+            for (int j = i + 1; j < arrayList2.size(); j++) {
+                if (value1 == arrayList2.get(j)) {
+                    Collections.swap(arrayList2, k + 1, j);
+                }
+            }
+        }
+        System.out.println("Result " + arrayList2);
+        return abc;
     }
 }
