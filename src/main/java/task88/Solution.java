f60c92b8355a2cdb2fa85eea6e9ae9475b7429be
mxnv <vadim.beller@mail.ru>
2024-06-14 17:01:31 +0300

    master - rework usual array#2

--- src/main/java/task88/Solution.java
+++ src/main/java/task88/Solution.java
@@ -10,23 +10,6 @@ public class Solution {
 
     private int counter = 0;
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        /*ArrayList<Integer> arrayList1 = new ArrayList<>();
-        ArrayList<Integer> arrayList2 = new ArrayList<>();
-
-        for(int a = 0; a < nums1.length; a++){
-            arrayList1.add(nums1[a]);
-        }
-        System.out.println(arrayList1);
-
-        for(int b = 0; b < nums2.length; b++){
-            arrayList2.add(nums2[b]);
-        }
-        System.out.println(arrayList2);*/
-
-
-       /* arrayList1 = Arrays.stream(nums1).boxed().collect(Collectors.toCollection(ArrayList::new));
-        arrayList2 = Arrays.stream(nums2).boxed().collect(Collectors.toCollection(ArrayList::new));*/
-
         for (int i = 0; i < nums1.length; i++){
             if(nums1[i] == 0){
                 for (int j = 0 + counter; j < nums2.length; j++){
@@ -38,7 +21,6 @@ public class Solution {
         }
         Arrays.sort(nums1);
         System.out.println(Arrays.toString(nums1));
-       /// nums1 = arrayList1.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 
