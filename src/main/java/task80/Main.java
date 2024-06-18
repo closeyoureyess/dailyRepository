015b700581710768fc8b1f42bd25b91e57543544
mxnv <vadim.beller@mail.ru>
2024-06-18 11:05:36 +0300

    master_80 - fixed the sorting

--- src/main/java/task80/Main.java
+++ src/main/java/task80/Main.java
@@ -1,9 +1,21 @@
 package task80;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
         solution.removeDuplicates(new int[]{0,0,1,1,1,1,2,3,3});
+       /* int enumerator = 0;
+        int[] a = {0,0,1,1,1,1,2,3,3};
+        for (int m = (a.length - 1) - 1; m >= 0; m--) {
+            if (a[a.length - 1] == a[m]) {
+                enumerator++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(enumerator);*/
     }
 }
--- src/main/java/task80/Solution.java
+++ src/main/java/task80/Solution.java
@@ -1,27 +1,101 @@
 package task80;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Solution {
     public int removeDuplicates(int[] nums) { // 0,0,1,1,1,1,2,3,3
         int j = 0;
+        int counter = 0;
+        int result = 0;
+        int enumerator = 0;
         int k = 0;
+        LinkedList<Integer> queueList = new LinkedList<>();
+
+        for (int p = 0; p < nums.length; p++) {
+            //Счетчик того, сколько дублей элемента в массиве, напр. 1 - 3
+            if (p != 0 && p != nums.length - 1 && nums[p] != nums[p + 1] && nums[p - 1] != nums[p]) {
+                queueList.push(nums[p]);
+            }
+            if (p == nums.length - 1 && nums[p] != nums[p - 1]) {
+                queueList.push(nums[p]);
+            }
+        }
+
+        for (int m = (nums.length - 1) - 1; m >= 0; m--) {
+            if (nums[nums.length - 1] == nums[m]) {
+                enumerator++;
+            } else {
+                break;
+            }
+        }
+
         for (int i = 0; i < nums.length; i++) {
+
             if (i != nums.length - 1 && nums[i] == nums[i + 1]) {
+                if (i != 0 && nums[i - 1] != nums[i]) {
+                    j = 0;
+                }
                 j++;
             } else {
-                if (j >= 2) {
+                if (i != nums.length - 1 && j == 2) {
+                    if (!queueList.isEmpty() && queueList.contains(nums[i]) && nums[i] == queueList.get(queueList.indexOf(nums[i]))) {
+                        int local = i;
+                        nums[i] = nums[i + 1];
+                        --j;
+                        nums[--local] = nums[i];
+                        //queueList.poll();
+                        i = 0;
+                    } else {
+                        nums[i] = nums[i + 1];
+                        counter++;
+                        j = 0;
+                    }
+                } else if (i != nums.length - 1 && j > 2) {
+                    int local = i;
+                    if (!queueList.isEmpty() && queueList.contains(nums[i]) && nums[i] == queueList.get(queueList.indexOf(nums[i]))) {
+                        while (j > 0) {
+                            --j;
+                            nums[--local] = nums[i];
+                            //queueList.poll();
+                        }
+                        i = 0;
+                    } else {
+                        nums[i] = nums[i + 1];
+                        while (j > 2) {
+                            nums[--local] = nums[i];
+                            j--;
+                        }
+                        j = 0;
+                        i = 0;
+                    }
+                } else if (i != nums.length - 1 && j < 2 && counter > 0) {
                     nums[i] = nums[i + 1];
-                    k++;
-                    j = 0;
-                } else if (j < 2 && k > 0) {
+                } else if (!queueList.isEmpty()
+                        && queueList.contains(nums[i])
+                        && nums[i] == queueList.get(queueList.indexOf(nums[i]))
+                        && i != nums.length - 1 && i != 0
+                        && j < 2 &&
+                        nums[i] == nums[i - 1] & nums[i] == nums[i + 1]) {
                     nums[i] = nums[i + 1];
-                    k = 0;
                 }
             }
+        }
+
+        for (int m = 0; m < nums.length; m++) {
+            if (nums[m] == nums[nums.length - 1]) {
+                break;
+            } else {
+                result++;
+            }
+        }
+
+        if (result > 1) {
+            k = result + 2;
+        } else {
+            k = result + 1;
         }
         System.out.println(Arrays.toString(nums));
         return k;
     }
-    //System.out.println(Arrays.toString(nums));
 }
