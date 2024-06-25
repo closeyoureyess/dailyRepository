861316edb4c535332705545d5de1dbcaf4aad6b6
mxnv <vadim.beller@mail.ru>
2024-06-25 17:13:56 +0300

    rework

--- src/main/java/threads21062024/Main.java
+++ src/main/java/threads21062024/Main.java
@@ -1,19 +1,75 @@
 package threads21062024;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
-    private static int b;
+    private static int count;
+
+    public static int getCount() {
+        return count;
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        Thread threadOne = new Thread(new Stream());
-        Thread threadTwo = new Thread(new Stream());
-        Thread threadThree = new Thread(new Stream());
         Student student = new Student();
-        student.createStudent(100);
+        List<Student> roster = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            student.setId(i);
+            roster.add(new Student(student.getId()));
+        }
+
+        // Вывод от 0 до 99
+        for (int i = 0; i < 100; i++) {
+            System.out.println(roster.get(i).getId());
+        }
+
+        //Прибавление тремя потоками +1 к каждому числу коллекции
+        Thread threadOne = new Thread(new Stream() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                for (int i = 0; i < 33; i++) {
+                    if (i <= (roster.size() - 1) / 3) {
+                        System.out.println(roster.get(count).getId() + 1);
+                        count++;
+                    }
+                }
+            }
+        });
+        Thread threadTwo = new Thread(new Stream() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                for (int i = 0; i < 33; i++) {
+                    if (i <= (roster.size() - 1) / 3) {
+                        System.out.println(roster.get(count).getId() + 1);
+                        count++;
+                    }
+                }
+            }
+        });
+        Thread threadThree = new Thread(new Stream() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                for (int i = 0; i <= 33; i++) {
+                    if (i <= (roster.size() - 1) / 3) {
+                        System.out.println(roster.get(count).getId() + 1);
+                        count++;
+                    }
+                }
+            }
+        });
+
         threadOne.start();
+        threadOne.join();
         threadTwo.start();
+        threadTwo.join();
         threadThree.start();
+        threadThree.join();
+
     }
 }
--- src/main/java/threads21062024/Student.java
+++ src/main/java/threads21062024/Student.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
-
-    private static int count;
     private int id;
-    private static List<Student> roster = new ArrayList<>();
 
     public Student(int id) {
         this.id = id;
@@ -16,41 +13,11 @@ public class Student {
     public Student() {
     }
 
-    public void createStudent(int students) {
-        for (int i = 0; i < students; i++) {
-            setId(i);
-            roster.add(new Student(getId()));
-        }
-        System.out.println(roster);
-    }
-
-    public static void modifyStudent() {
-        synchronized (Student.class) {
-            int i = 0;
-            for (int k = count; k < roster.size(); k++) {
-                if (i <= (roster.size() - 1) / 3) {
-                    String a = String.valueOf(roster.get(k));
-                    String b = a.substring(a.indexOf("=") + 1, a.length() - 1);
-                    System.out.println(Integer.parseInt(b) + 1);
-                    count = k + 1;
-                    i++;
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" + "id=" + id + '}';
     }
 }
