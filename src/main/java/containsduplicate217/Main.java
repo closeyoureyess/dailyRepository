6068552fd76e85754dcf3fa86147ef5569186286
vsirik <vsirik@niisokb.ru>
2024-12-03 06:10:44 +0700

    contains duplicate 217

--- src/main/java/containsduplicate217/Main.java
+++ src/main/java/containsduplicate217/Main.java
@@ -2,6 +2,8 @@ package containsduplicate217;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        boolean result = solution.containsDuplicate(new int[]{1,2,3,4,});
+        System.out.println(result);
     }
 }
--- src/main/java/containsduplicate217/Solution.java
+++ src/main/java/containsduplicate217/Solution.java
@@ -1,7 +1,33 @@
 package containsduplicate217;
 
-class Solution {
-    public void containsDuplicate(int[] nums) {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        Integer value;
+        int pointer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            value = nums[i];
+            if (map.isEmpty()) {
+                pointer++;
+                map.put(value, value);
+            }
+            if (map != null && pointer == 0) {
+                boolean boolValue = map.containsKey(value);
+                if (boolValue) {
+                    return true;
+                }
+                map.put(value, value);
+            }
+            pointer = 0;
+        }
+        return false;
     }
 }
+
+//Учитывая целочисленный массив nums, верните true, если какое-либо значение появляется в массиве по крайней мере дважды,
+// и верните false, если каждый элемент различен.
